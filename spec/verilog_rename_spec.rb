353fa3cca1cca6a13e190344807a27c977d0afc7
Morgan Prior <morgan@amaras-tech.co.uk>
2011-07-03 22:01:48 +0100

    Improved test to copy the fixtures into a temp folder

--- spec/verilog_rename_spec.rb
+++ spec/verilog_rename_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
+require 'fileutils'
 require 'pp'
 
 describe VerilogRename do
 
   it "Open file" do
     path = File.dirname( __FILE__ )
+
+    ## Git cant have empty folders, so create in test
+    FileUtils.mkdir_p( ::File.join( path, 'scratch') )
+
+    ## Clean Scratch Area
+    FileUtils.rm( Dir.glob( ::File.join( path, 'scratch', '*.*') ) )
+
+    ## Copy files from fixtures to scratch
+    FileUtils.cp_r( ::File.join( path, 'fixtures'), ::File.join( path, 'scratch' ) )
 
     #test_one = Verilog::File.new("test_one.v", {:path => File.join( path, 'fixtures') })
     #test_one.read_from_disk
@@ -16,7 +26,7 @@ describe VerilogRename do
     #test_four.read_from_disk
 
 
-    path_files = Verilog::PathFiles.new( File.join( path, 'fixtures') )
+    path_files = Verilog::PathFiles.new( File.join( path, 'scratch', 'fixtures') )
     path_files.read_all
 
     rename = Verilog::Rename.new('TEST_ONE', 'TEST_TWO', path_files)
