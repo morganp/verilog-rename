8ac5507ddb6f4520963bc4ab7c1bbce190d1fe62
Morgan Prior <morgan@amaras-tech.co.uk>
2011-10-25 22:40:58 +0100

    Version number bump 0.0.2 -> 0.1.0 due to interface change.

--- lib/verilog_rename.rb
+++ lib/verilog_rename.rb
@@ -3,7 +3,7 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 module VerilogRename
-  VERSION = '0.0.2'
+  VERSION = '0.1.0'
 end
 
 require 'verilog'
